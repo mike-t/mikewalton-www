76c21dc2c815517767a765e1ce0a75ef55592786
Mike Walton <727393+mike-t@users.noreply.github.com>
2023-05-25 20:27:08 +1000

    Changes for Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import "~/styles/globals.css";
 
-const MyPortfolioApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-  <Analytics />
-};
-
+function MyPortfolioApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
 export default MyPortfolioApp;
